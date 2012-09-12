c75f9de4662bf897a7f459e509f99bd5315af55a
TJ Holowaychuk <tj@vision-media.ca>
2012-09-12 14:57:10 -0700

    docs

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 
-module.exports = function(str, obj){
+/**
+ * Expose `t`.
+ */
+
+module.exports = t;
+
+/**
+ * Translate the given `str` with substitions
+ * provided in `obj`.
+ *
+ * @param {String} str
+ * @param {Object} obj
+ * @return {String}
+ * @api public
+ */
+
+function t(str, obj){
   obj = obj || {};
   return str.replace(/\{([^}]+)\}/g, function(_, name){
     return obj[name] || _;
   });
-};
+}
