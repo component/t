daca31f2d53d53a767730a667683a178bb4c6e81
TJ Holowaychuk <tj@vision-media.ca>
2012-09-12 14:55:32 -0700

    add substitution support

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
-module.exports = function(str){
-  return str;
+module.exports = function(str, obj){
+  obj = obj || {};
+  return str.replace(/\{([^}]+)\}/, function(_, name){
+    return obj[name] || _;
+  });
 };
--- test/index.js
+++ test/index.js
@@ -5,4 +5,9 @@ describe('t(str)', function(){
   it('should return the english string', function(){
     t('Hello').should.equal('Hello');
   })
+
+  it('should replace tokens', function(){
+    t('Hello {name}', { name: 'Tobi' }).should.equal('Hello Tobi');
+    t('Hello {name}').should.equal('Hello {name}');
+  })
 })
